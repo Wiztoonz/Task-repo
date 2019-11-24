3afd9533cdf0bb4cfde93994431d36da57e56d12
Rosynskyi Oleg <olegrosynskyi@gmail.com>
2019-11-24 18:06:45 +0200

    add build recursion method

--- task2.1/src/main/java/com/program/task2/NewRecursion.java
+++ task2.1/src/main/java/com/program/task2/NewRecursion.java
@@ -4,20 +4,24 @@ public class NewRecursion {
     public String changeString(String str) {
         StringBuilder changeBuilder = new StringBuilder();
         char[] vowels = {'a', 'e', 'i', 'o', 'u'};
-        int indexVowels = 0;
+        int indexVowels = 0, indexStr = 0;
+        return build(changeBuilder, str, indexStr, vowels, indexVowels);
+    }
 
-        for (int i = 0; i < str.length(); i++) {
-            changeBuilder.append(str.charAt(i));
-            if (i < str.length() - 1) {
-                if (isVowel(str.charAt(i), vowels, indexVowels) && str.charAt(i + 1) != ' ') {
-                    changeBuilder.append("*");
-                }
-                if (isVowel(str.charAt(i + 1), vowels, indexVowels) && str.charAt(i) != ' ') {
-                    changeBuilder.append("*");
-                }
+    private String build(StringBuilder changeBuilder, String str, int indexStr, char[] vowels, int indexVowels) {
+        changeBuilder.append(str.charAt(indexStr));
+        if (indexStr < str.length() - 1) {
+            if (isVowel(str.charAt(indexStr), vowels, indexVowels) && str.charAt(indexStr + 1) != ' ') {
+                changeBuilder.append("*");
+            }
+            if (isVowel(str.charAt(indexStr + 1), vowels, indexVowels) && str.charAt(indexStr) != ' ') {
+                changeBuilder.append("*");
             }
         }
-        return new String(changeBuilder).replaceAll("\\*\\*", "\\*");
+        indexStr++;
+        return (str.length() == indexStr)
+                ? new String(changeBuilder).replaceAll("\\*\\*", "\\*")
+                : build(changeBuilder, str, indexStr, vowels, indexVowels);
     }
 
     private boolean isVowel(char symbol, char[] vowels, int indexVowels) {
